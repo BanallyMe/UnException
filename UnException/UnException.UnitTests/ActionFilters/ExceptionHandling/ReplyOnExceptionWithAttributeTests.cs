424e30bcc0188abc29811912369c934cc116df41
BanallyMe <admin@eggrade.de>
2020-05-19 13:14:27 +0200

    Introduce attribute to handle action exceptions

---
+++ UnException/UnException.UnitTests/ActionFilters/ExceptionHandling/ReplyOnExceptionWithAttributeTests.cs
@@ -0,0 +1,39 @@
+﻿using BanallyMe.UnException.ActionFilters.ExceptionHandling;
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace BanallyMe.UnException.UnitTests.ActionFilters.ExceptionHandling
+{
+    public class ReplyOnExceptionWithAttributeTests
+    {
+        [Fact]
+        public void Constructing_ThrowsIfExceptionTypeIsNull()
+        {
+            Action attributeConstruction = () => new ReplyOnExceptionWithAttribute(null, 200);
+
+            attributeConstruction.Should().ThrowExactly<ArgumentNullException>().Which.ParamName.Should().Be("exceptionType");
+        }
+
+        [Fact]
+        public void Constructing_ThrowsIfExceptionTypeIsNoException()
+        {
+            Action attributeConstruction = () => new ReplyOnExceptionWithAttribute(typeof(ReplyOnExceptionWithAttributeTests), 200);
+
+            attributeConstruction.Should().ThrowExactly<ArgumentException>().Which.ParamName.Should().Be("exceptionType");
+        }
+
+        [Theory]
+        [InlineData(-200)]
+        [InlineData(0)]
+        [InlineData(99)]
+        [InlineData(600)]
+        public void Constructing_ThrowsIfHttpStatusCodeIsInvalid(int invalidTestStatuscode)
+        {
+            Action attributeConstruction = () => new ReplyOnExceptionWithAttribute(typeof(Exception), invalidTestStatuscode);
+
+            attributeConstruction.Should().ThrowExactly<ArgumentOutOfRangeException>()
+                .Which.Should().Match<ArgumentOutOfRangeException>(exc => exc.ParamName == "httpStatusCode" && (int)exc.ActualValue == invalidTestStatuscode);
+        }
+    }
+}
---
+++ UnException/UnException/ActionFilters/ExceptionHandling/ReplyOnExceptionAttribute.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace BanallyMe.UnException.ActionFilters.ExceptionHandling
+{
+    /// <summary>
+    /// Defines a simple HTTP-reply that is sent to the client if the decorated action threw
+    /// an exception of the specified type.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method)]
+    public sealed class ReplyOnExceptionWithAttribute : Attribute
+    {
+        /// <summary>
+        /// Type of exception that will be automatically replied on.
+        /// </summary>
+        public Type ExceptionType { get; }
+        /// <summary>
+        /// HTTP-Statuscode of the automatically generated reply.
+        /// </summary>
+        public int HttpStatusCode { get; }
+        /// <summary>
+        /// Message of the automatically generated reply. If this is set to null, the message of the
+        /// occured exception will be used (default behaviour).
+        /// </summary>
+        public string? ReplyMessage { get; set; } = null;
+
+        /// <summary>
+        /// If set to true, the exception will be passed to the ASP.NET Core logging API, otherwise
+        /// the exception will simply be discarded.
+        /// </summary>
+        public bool LogException { get; set; } = true;
+
+
+        /// <summary>
+        /// Defines a simple HTTP-reply for the decorated action in case of the specified exception.
+        /// </summary>
+        /// <param name="exceptionType">Type of exception that will be automatically replied on.</param>
+        /// <param name="httpStatusCode">HTTP-Statuscode of the automatically generated reply.</param>
+        public ReplyOnExceptionWithAttribute(Type exceptionType, int httpStatusCode)
+        {
+            if (exceptionType is null) throw new ArgumentNullException(nameof(exceptionType));
+            if (!TypeIsException(exceptionType)) throw new ArgumentException("Exception type must derive of System.Exception", nameof(exceptionType));
+            if (!StatusCodeIsInValidRange(httpStatusCode)) throw new ArgumentOutOfRangeException(nameof(httpStatusCode), httpStatusCode, "Statuscode must be a value between 100 and 599.");
+
+            ExceptionType = exceptionType;
+            HttpStatusCode = httpStatusCode;
+        }
+
+        private bool TypeIsException(Type typeCandidate)
+            => typeCandidate == typeof(Exception) || typeCandidate.IsSubclassOf(typeof(Exception));
+
+        private bool StatusCodeIsInValidRange(int statusCode)
+            => statusCode >= 100 && statusCode < 600;
+    }
+}
