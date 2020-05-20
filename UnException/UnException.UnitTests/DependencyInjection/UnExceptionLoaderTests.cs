aafa4f5252dc044824d89df5ed3964d387d25bb7
BanallyMe <admin@eggrade.de>
2020-05-20 14:46:25 +0200

    Extension method to register filter in MVC

---
+++ UnException/UnException.UnitTests/DependencyInjection/UnExceptionLoaderTests.cs
@@ -0,0 +1,52 @@
+﻿using BanallyMe.UnException.ActionFilters.ExceptionHandling;
+using BanallyMe.UnException.DependencyInjection;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace BanallyMe.UnException.UnitTests.DependencyInjection
+{
+    public class UnExceptionLoaderTests
+    {
+        [Fact]
+        public void AddUnException_AddsReplyOnActionWithFilterToMvcConfiguration()
+        {
+            var fakeServiceCollection = new ServiceCollection();
+
+            fakeServiceCollection.AddUnException();
+
+            var mvcConfig = GetMvcConfigurationFromServices(fakeServiceCollection);
+            mvcConfig.Should().NotBeNull();
+            var configurationAction = GetConfigurationActionFromService(mvcConfig);
+            AssertConfigurationActionAddsReplyOnExceptionWithFilter(configurationAction);
+        }
+
+        private ServiceDescriptor GetMvcConfigurationFromServices(IServiceCollection services)
+            => services.FirstOrDefault(service => service.ServiceType == typeof(IConfigureOptions<MvcOptions>));
+
+        private Action<MvcOptions> GetConfigurationActionFromService(ServiceDescriptor service)
+            => ((ConfigureNamedOptions<MvcOptions>)service.ImplementationInstance).Action;
+
+        private void AssertConfigurationActionAddsReplyOnExceptionWithFilter(Action<MvcOptions> configurationAction)
+        {
+            var fakeOptions = new MvcOptions();
+
+            configurationAction.Invoke(fakeOptions);
+
+            AssertMvcOptionsContainReplyOnExceptionWithFilter(fakeOptions);
+        }
+
+        private void AssertMvcOptionsContainReplyOnExceptionWithFilter(MvcOptions mvcOptions)
+        {
+            var x = mvcOptions.Filters.Select(filter => filter.GetType());
+
+            mvcOptions.Filters.OfType<TypeFilterAttribute>()
+                .Where(attr => attr.ImplementationType == typeof(ReplyOnExceptionWithFilter))
+                .Should().HaveCount(1);
+        }
+    }
+}
---
+++ UnException/UnException/DependencyInjection/UnExceptionLoader.cs
@@ -0,0 +1,25 @@
+﻿using BanallyMe.UnException.ActionFilters.ExceptionHandling;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BanallyMe.UnException.DependencyInjection
+{
+    /// <summary>
+    /// Provides extension methods to add the UnException library to the .NET Core
+    /// dependency injection container.
+    /// </summary>
+    public static class UnExceptionLoader
+    {
+        /// <summary>
+        /// Adds the UnException action filters to the ASP.NET Core MVC framework.
+        /// </summary>
+        /// <param name="services">Collection of services for building a dependency injection container.</param>
+        public static void AddUnException(this IServiceCollection services)
+        {
+            services.Configure<MvcOptions>(options =>
+            {
+                options.Filters.Add<ReplyOnExceptionWithFilter>();
+            });
+        }
+    }
+}
