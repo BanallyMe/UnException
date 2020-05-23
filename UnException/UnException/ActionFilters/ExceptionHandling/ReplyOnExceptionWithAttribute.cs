3f73fb6a24a6b13a3622b2ae63b49fcfccfbe0da
BanallyMe <admin@eggrade.de>
2020-05-23 08:33:06 +0200

    Added a property for API description

--- UnException/UnException/ActionFilters/ExceptionHandling/ReplyOnExceptionWithAttribute.cs
+++ UnException/UnException/ActionFilters/ExceptionHandling/ReplyOnExceptionWithAttribute.cs
@@ -14,15 +14,23 @@ namespace BanallyMe.UnException.ActionFilters.ExceptionHandling
         /// Type of exception that will be automatically replied on.
         /// </summary>
         public Type ExceptionType { get; }
+
         /// <summary>
         /// HTTP-Statuscode of the automatically generated reply.
         /// </summary>
         public int HttpStatusCode { get; }
+
         /// <summary>
         /// Message of the automatically generated reply. If this is set to null, the message of the
         /// occured exception will be used (default behaviour).
         /// </summary>
         public string? ReplyMessage { get; set; } = null;
+
+        /// <summary>
+        /// A description of when and why this Exception will occur. This can be used by
+        /// API descriptors to document this exception response.
+        /// </summary>
+        public string? ErrorDescription { get; set; } = null;
 
         /// <summary>
         /// If set to true, the exception will be passed to the ASP.NET Core logging API, otherwise
